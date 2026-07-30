1475e5204b3f2b5c7a36e7b7370b1fed030fd003
childofyah11-bit <childofyah11@gmail.com>
2026-07-30 16:30:20 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,5 @@
+// This file runs in a dedicated thread to survive background tab sleeping
+setInterval(() => {
+    // Post a message back to the main website window to trigger the network pulse
+    self.postMessage('pulse');
+}, 10000); // Strict 10-second background maintenance loop
